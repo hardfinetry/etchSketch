8065a0bf6b8bb3bb8e186956c1789d342fb57a81
hardfinetry <221923048+hardfinetry@users.noreply.github.com>
2026-05-26 15:12:45 +0800

    allow users to define size of grid

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 const container = document.querySelector("#container");
-// const div = document.createElement("div");
-for (let i = 0; i < 256; i++) {
+
+function createGrid(numOfSquares) {
+    container.innerHTML = "";
+    for (let i = 0; i < numOfSquares * numOfSquares; i++) {
     const div = document.createElement("div");
+    div.style.width = 100 / numOfSquares + "%";
+    div.style.height = 100 / numOfSquares + "%";
     container.appendChild(div)
 }
+};
+
+createGrid(16);
+
+const button = document.querySelector("#button");
+
+button.addEventListener("click", () => {
+    let numOfSquares = prompt("How many squares per side?");
+    numOfSquares = Number(numOfSquares);
+
+    if (Number.isInteger(numOfSquares) &&
+        numOfSquares > 0 &&
+        numOfSquares <= 100) {
+       createGrid(numOfSquares);
+    }
+    else {
+        alert("Please enter a number greater than 0 and less than 100!");
+    }
+});
