d5c790295d317c30c738f1bd7659bbe877d61baf
hardfinetry <221923048+hardfinetry@users.noreply.github.com>
2026-05-26 14:26:10 +0800

    add 16x16 grid

---
+++ script.js
@@ -0,0 +1,6 @@
+const container = document.querySelector("#container");
+// const div = document.createElement("div");
+for (let i = 0; i < 256; i++) {
+    const div = document.createElement("div");
+    container.appendChild(div)
+}
