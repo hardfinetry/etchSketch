838518fbf8998cd43910e256338b2ade42072701
hardfinetry-whitey <221923048+hardfinetry@users.noreply.github.com>
2026-05-27 11:33:08 +0800

    add random color and progressive opacity

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
 const container = document.querySelector("#container");
+const button = document.querySelector("#button");
+const tiles = document.querySelectorAll(".tiles")
+
+function randomColour() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r}, ${g}, ${b})`;
+};
 
 function createGrid(numOfSquares) {
     container.innerHTML = "";
     for (let i = 0; i < numOfSquares * numOfSquares; i++) {
     const div = document.createElement("div");
+    div.classList.add("tiles");
     div.style.width = 100 / numOfSquares + "%";
     div.style.height = 100 / numOfSquares + "%";
+    div.dataset.darkness = 0;
+
+    div.addEventListener("mouseenter", () => {
+        let darkness = Number(div.dataset.darkness);
+
+        if (!div.style.backgroundColor) {
+            div.style.backgroundColor = randomColour();
+        }
+
+        if (darkness < 10) {
+            darkness++;
+            div.dataset.darkness = darkness;
+            div.style.opacity = darkness / 10;
+        }
+        });
+
     container.appendChild(div)
 }
 };
 
 createGrid(16);
-
-const button = document.querySelector("#button");
 
 button.addEventListener("click", () => {
     let numOfSquares = prompt("How many squares per side?");
